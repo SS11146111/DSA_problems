f91b94ff1e24d4e6f8f177490f4bde7219f12483
SS11146111 <sahasangita59@gmail.com>
2024-06-29 13:31:35 +0530

    updated insertion sort

---
+++ insertionSort.js
@@ -0,0 +1,20 @@
+const arr = [19,2,3,17,23,10]
+let n = arr.length;
+
+function insertionSort(arr,n)
+{
+    let key, j;
+    for(let i=1;i<n;i++)
+        {
+            key = arr[i]
+            j=i-1;
+            while(j>=0 && arr[j]> key){
+                arr[j+1] = arr[j]
+                j--;
+            }
+            arr[j+1] = key;
+        }
+    return arr;
+}
+
+console.log(insertionSort(arr,n))
